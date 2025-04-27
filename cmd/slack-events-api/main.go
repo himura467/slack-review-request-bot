b862ae76ec1f515796b6a85bc92f5346f92c83e7
himura467 <mitarashidango0927@gmail.com>
2025-04-28 05:03:50 +0900

    feat: log

--- cmd/slack-events-api/main.go
+++ cmd/slack-events-api/main.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 )
 
 func main() {
+	log.Print("Starting Slack Events API server")
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
 
-	http.ListenAndServe(":8080", r)
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
