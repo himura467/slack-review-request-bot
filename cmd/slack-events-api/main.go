c4ab3e4e82f1d50551c2a76c089f345aed01175f
himura467 <mitarashidango0927@gmail.com>
2025-04-28 05:05:05 +0900

    feat: use environment variable $PORT

--- cmd/slack-events-api/main.go
+++ cmd/slack-events-api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -10,13 +11,19 @@ import (
 func main() {
 	log.Print("Starting Slack Events API server")
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World"))
 	})
 
-	if err := http.ListenAndServe(":8080", r); err != nil {
+	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatal(err)
 	}
 }
