3f6d0f24c3774364c1acc4139d1dbb1a6990c194
himura467 <mitarashidango0927@gmail.com>
2025-05-01 05:58:08 +0900

    feat: implement slack events api main package

--- cmd/slack-events-api/main.go
+++ cmd/slack-events-api/main.go
@@ -1,29 +1,6 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/go-chi/chi/v5"
-)
-
 func main() {
-	log.Print("Starting Slack Events API server")
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
-	}
-
-	r := chi.NewRouter()
-
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
-
-	if err := http.ListenAndServe(":"+port, r); err != nil {
-		log.Fatal(err)
-	}
+	app := initializeApp()
+	app.Run()
 }
