13225aec757b2f71ccf2ccd3b00c8b82d5576e71
himura467 <mitarashidango0927@gmail.com>
2025-06-16 02:12:24 +0900

    refactor: unify shell style

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -e
 ROOT_DIR=$(cd "$(dirname "$0")"/..; pwd)
 
 if [[ ! -f .env ]]; then
-  bash "${ROOT_DIR}"/scripts/setup.sh
+  bash "$ROOT_DIR/scripts/setup.sh"
 fi
 
 source .env
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ export CLOUDSDK_ACTIVE_CONFIG_NAME=$2
 
 ROOT_DIR=$(cd "$(dirname "$0")"/..; pwd)
 
-cd "$ROOT_DIR"/terraform
+cd "$ROOT_DIR/terraform"
 
 terraform init
 # image の tag を latest に固定しているため、一度 destroy しないと更新されない
